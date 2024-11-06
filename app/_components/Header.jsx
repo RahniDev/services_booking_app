39891543f029b48c85fd3fc2cc63e75e3de0435d
RahniDev <rahni32@protonmail.com>
2024-11-06 14:34:03 +0100

    Create header

---
+++ app/_components/Header.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import {Button} from '@/components/ui/button'
+
+const Header = () => {
+  return (
+    <div className="p-5 shadow-sm flex justify-between">
+    <div className='flex items-center gap-8'>
+        <Image src='/logo.svg' alt='logo' width={30} height={30} />
+        <ul className='md:flex items-center gap-6 hidden'>
+            <li className='hover:scale-105 hover:text-primary'>Home</li>
+            <li className='hover:scale-105 hover:text-primary'>Services</li>
+            <li className='hover:scale-105 hover:text-primary'>About Us</li>
+        </ul>
+    </div>
+    <div><Button>Get Started</Button></div>
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#007DFC',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
