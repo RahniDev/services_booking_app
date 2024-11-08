ff35e1c131473e80d90338e09d67386510c7a7de
RahniDev <rahni32@protonmail.com>
2024-11-08 22:53:25 +0100

    Write GraphQL category query

---
+++ app/_services/GlobalApi.js
@@ -0,0 +1,21 @@
+const MASTER_URL = 'https://eu-west-2.cdn.hygraph.com/content/' + process.env.NEXT_PUBLIC_MASTER_URL_KEY + '/master'
+
+const getCategory = async () => {
+    const query = gql`
+    query Category {
+  categories {
+    id
+    name
+    icon {
+      url
+    }
+  }
+}`
+
+    const result = await request(MASTER_URL, query)
+    return result
+}
+
+export default {
+    getCategory
+}
