7b282810aa32c8dbf18fcdb7df46f6856d71d955
RahniDev <rahni32@protonmail.com>
2024-11-26 00:42:49 +0100

    Add gallery section to business page

--- app/(routes)/details/[businessId]/page.jsx
+++ app/(routes)/details/[businessId]/page.jsx
@@ -23,8 +23,8 @@ const BusinessDetails = () => {
         <div className="py-8 md:py-20 px-10 md:px-36">
             <BusinessInfo business={business} />
 
-               <div className="grid grid-cols-4 mt-8">
-                <div className="col-span-3">
+               <div className="grid grid-cols-4 mt-16">
+                <div className="col-span-4 md:col-span-3">
                 <BusinessDescription business={business} />
                 </div>
                 <div className="hidden md:block">
