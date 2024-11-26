3e24418115de219e2b72a5075f86449b7a570cec
RahniDev <rahni32@protonmail.com>
2024-11-26 15:30:06 +0100

    Implement time slot functionality on calendar

--- app/(routes)/details/_components/BookingAppointment.jsx
+++ app/(routes)/details/_components/BookingAppointment.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {Button} from '@/components/ui/button'
 import {
     Sheet,
     SheetContent,
@@ -11,17 +12,45 @@ import { Calendar } from "@/components/ui/calendar"
 
 const BookingAppointment = ({ children }) => {
     const [date, setDate] = useState(new Date())
+    const [timeSlot, setTimeSlot] = useState([])
+
+    useEffect(() => {
+        getTime()
+    }, [])
+    const getTime = () => {
+        const timeList = [];
+        for (let i = 10; i <= 12; i++) {
+            timeList.push({
+                time: i + ':00 AM'
+            })
+            timeList.push({
+                time: i + ':30 AM'
+            })
+        }
+        for (let i = 1; i <= 6; i++) {
+            timeList.push({
+                time: i + ':00 PM'
+            })
+            timeList.push({
+                time: i + ':30 PM'
+            })
+        }
+
+        setTimeSlot(timeList)
+    }
+
+
     return (
         <div>
             <Sheet>
                 <SheetTrigger asChild>{children}</SheetTrigger>
-                <SheetContent>
+                <SheetContent className="overflow-auto">
                     <SheetHeader>
                         <SheetTitle>Book Service</SheetTitle>
                         <SheetDescription>
                             Select a date and time for your appointment.
                             <div className='flex flex-col gap-5 items-baseline'>
-                                <h2>Select Date</h2>
+                                <h2 className='my-5 font-bold'>Select Date</h2>
                             </div>
                             <div>
                                 <Calendar
@@ -30,7 +59,17 @@ const BookingAppointment = ({ children }) => {
                                     onSelect={setDate}
                                     className="rounded-md border"
                                 />
-                            </div>
+                                  </div>
+                                  <h2 className='my-5 font-bold'>Select Time Slot</h2>
+                                <div className='grid grid-cols-3 gap-3'>
+                                {timeSlot.map((item, index) => (
+                                    <Button key={index}
+                                    variant='outline'
+                                    className='border rounded-full p-2 px-3'>
+                                        {item.time}
+                                    </Button>
+                                ))}
+                                </div>
                         </SheetDescription>
                     </SheetHeader>
                 </SheetContent>
