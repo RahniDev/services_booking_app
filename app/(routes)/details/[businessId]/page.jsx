1615d52a35a785351ecc5dd3282a58280c28f3c7
RahniDev <rahni32@protonmail.com>
2024-11-26 01:29:47 +0100

    Create suggested services section

--- app/(routes)/details/[businessId]/page.jsx
+++ app/(routes)/details/[businessId]/page.jsx
@@ -23,14 +23,15 @@ const BusinessDetails = () => {
         <div className="py-8 md:py-20 px-10 md:px-36">
             <BusinessInfo business={business} />
 
-               <div className="grid grid-cols-4 mt-16">
-                <div className="col-span-4 md:col-span-3">
-                <BusinessDescription business={business} />
+            <div className="grid grid-cols-3 mt-16">
+                <div className="col-span-3 md:col-span-2
+                order-last md:order-first">
+                    <BusinessDescription business={business} />
                 </div>
-                <div className="hidden md:block">
-                <SuggestedServices business={business} />
+                <div>
+                    <SuggestedServices business={business} />
                 </div>
-                </div>
+            </div>
         </div>
     )
 }
--- app/(routes)/details/_components/SuggestedServices.jsx
+++ app/(routes)/details/_components/SuggestedServices.jsx
@@ -1,7 +1,55 @@
-const SuggestedServices = () => {
+import { useEffect, useState } from "react"
+import { Button } from '@/components/ui/button'
+import { NotebookPen } from "lucide-react"
+import GlobalApi from "@/app/_services/GlobalApi"
+import Image from "next/image"
+import Link from "next/link"
+
+const SuggestedServices = ({ business }) => {
+  const [businessList, setBusinessList] = useState([])
+
+  useEffect(() => {
+    business && getBusinessList()
+  }, [business])
+
+  const getBusinessList = () => {
+    GlobalApi.getBusinessByCategory(business?.category?.name)
+      .then(resp => {
+        setBusinessList(resp?.businessLists)
+      })
+  }
 
   return (
-    <div>SuggestedServices</div>
+    <div className="md:pl-10">
+      <Button className="flex gap-2 w-full">
+        <NotebookPen />
+        Book Appointment
+      </Button>
+      <div className="hidden md:block">
+        <h2 className="font-bold text-lg mt-3 mb-3">
+          Similar Businesses</h2>
+        <div>
+          {businessList && businessList.map((business, index) => (
+            <Link href={'/details/' + business.id}
+              className="flex gap-2 mb-4
+          cursor-pointer hover:shadow-md
+          hover:border border-primary rounded-lg p-2">
+              <Image src={business?.images[0].url}
+                alt={business.name}
+                width={80}
+                height={80}
+                className="rounded-lg object-cover"
+              />
+              <div>
+                <h3 className="font-bold">{business.name}</h3>
+                <h3 className="text-primary">{business.contactPerson}</h3>
+                <address className="text-gray-400">{business.address}</address>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
